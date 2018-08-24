1c4d4d7cd50bab3c66571a2d2d70110cef3c199c
Olivier Breuleux <breuleux@gmail.com>
2018-08-24 11:43:41 -0400

    0.1.7

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='hrepr',
-    version='0.1.6',
+    version='0.1.7',
 
     description='Extensible HTML representation for Python objects.',
     long_description=long_description,

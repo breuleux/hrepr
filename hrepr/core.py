99a2978d71fd16573c2f557b54effbf64a043ddf
Olivier Breuleux <breuleux@gmail.com>
2020-10-04 22:46:46 -0400

    Add new implementation based on ovld

---
+++ hrepr/core.py
@@ -0,0 +1,365 @@
+
+import textwrap
+from collections import Counter
+from dataclasses import is_dataclass, fields as dataclass_fields
+
+from ovld import ovld, OvldCall, OvldMC
+from .h import css_hrepr, HTML, Tag
+from . import std
+
+
+SHORT = object()
+
+
+# class DataclassMC(type):
+#     def __subclasscheck__(cls, sub):
+#         return (
+#             is_dataclass(sub) and not hasattr(sub, cls.excpt)
+#         )
+
+
+# class DataclassNoHrepr(metaclass=DataclassMC):
+#     pass
+
+
+# class DataclassNoHreprShort(metaclass=DataclassMC):
+#     pass
+
+
+class MetaMC(type):
+    def __subclasscheck__(cls, sub):
+        return cls.chk(sub)
+
+
+def meta(condition):
+    class M(metaclass=MetaMC):
+        @classmethod
+        def chk(cls, sub):
+            return condition(sub)
+    return M
+
+
+def dataclass_without(prop):
+    @meta
+    def fn(cls):
+        return is_dataclass(cls) and not hasattr(cls, prop)
+    return fn
+
+
+class Config:
+    def __init__(self, cfg={}, parent=None):
+        self._parent = parent
+        self.__dict__.update(cfg)
+
+    def __call__(self, **cfg):
+        return self.with_config(cfg)
+
+    def with_config(self, cfg):
+        # rval = copy(self)
+        # rval.__dict__.update(cfg)
+        # return rval
+        if not cfg:
+            return self
+        elif self.__dict__.keys() == cfg.keys():
+            return Config(cfg, self._parent)
+        else:
+            return Config(cfg, self)
+
+    def __getattr__(self, attr):
+        # Only triggers for attributes not in __dict__
+        if attr.startswith("_"):
+            return getattr(super(), attr)
+        elif self._parent:
+            return getattr(self._parent, attr)
+        return None
+
+    def __hrepr__(self, H, hrepr):
+        return hrepr.stdrepr_object("Config", self.__dict__.items())
+
+
+# class ResourceAccumulator:
+#     def __init__(self, acq):
+#         self.consulted = set()
+#         self.resources = set()
+#         self.spent = set()
+#         self.acq = acq
+
+#     def acquire(self, cls):
+#         if cls not in self.consulted:
+#             self.consulted.add(cls)
+#             resources = self.acq(cls)
+#             resources -= self.spent
+#             self.resources |= resources
+
+#     def dump(self):
+#         rval = self.resources
+#         self.spent |= self.resources
+#         self.resources.clear()
+#         return rval
+
+
+class HreprState:
+    def __init__(self, resources=set()):
+        self.types_seen = set()
+        self.resources = set(resources)
+        self.stack = Counter()
+        self.registry = {}
+        self.depth = -1
+        self.refs = {}
+
+    def get_ref(self, objid):
+        return self.refs.setdefault(objid, len(self.refs) + 1)
+
+    def registered(self, objid):
+        return objid in self.registry
+
+    def register(self, objid, value):
+        self.registry[objid] = value
+
+    def make_refmap(self):
+        rval = {}
+        for objid, label in self.refs.items():
+            rval[id(self.registry[objid])] = label
+        return rval
+
+
+class Hrepr(metaclass=OvldMC):
+
+    def __init__(self, H, config=None, master=None):
+        self.H = H
+        if config is None:
+            config = Config()
+        self.config = config
+        self.master = master or self
+        self.state = (
+            master.state
+            if master
+            else HreprState(self.global_resources())
+        )
+
+    def with_config(self, config):
+        if not config:
+            return self
+        else:
+            cfg = self.config.with_config(config)
+            return type(self)(H=self.H, config=cfg, master=self.master)
+
+    def ref(self, obj, loop=False):
+        # breakpoint()
+        num = self.state.get_ref(id(obj))
+        sym = "⟳" if loop else "#"
+        ref = self.H.span["hrepr-ref"](sym, num)
+        if self.config.shortref:
+            return ref
+        else:
+            return self.H.div["hrepr-refbox"](ref("="), self.hrepr_short(obj))
+            # short = self.hrepr_short(obj)
+            # if short is NotImplemented:
+            #     short = self.default_hrepr_short(obj)
+            # return self.H.div["hrepr-refbox"](ref("="), short)
+
+    def global_resources(self):
+        return set()
+
+    # def default_hrepr_resources(self, cls):
+    #     return None
+
+    def default_hrepr(self, obj):
+        clsn = type(obj).__name__
+        return self.H.span[f"hreprt-{clsn}"](str(obj))
+
+    def default_hrepr_short(self, obj):
+        clsn = type(obj).__name__
+        return self.H.span[f"hreprs-{clsn}"]("<", clsn, ">")
+
+    @ovld
+    def hrepr_resources(self, cls: object):
+        if hasattr(cls, "__hrepr_resources__"):
+            return cls.__hrepr_resources__(self.H)
+
+    @ovld.dispatch
+    def hrepr(ovldcall, obj):
+        self = ovldcall.obj
+        rval = ovldcall.resolve(obj)(obj)
+        if rval is SHORT:
+            return self.hrepr_short(obj)
+        elif rval is NotImplemented:
+            rval = self.hrepr_short.resolve(obj)(obj)
+            if rval is NotImplemented:
+                rval = self.default_hrepr(obj)
+            else:
+                return rval
+        self.state.register(id(obj), rval)
+        return rval
+
+    def hrepr(self, obj: object):
+        if hasattr(obj, "__hrepr__"):
+            return obj.__hrepr__(self, self.H)
+        else:
+            return NotImplemented
+
+    @ovld.dispatch
+    def hrepr_short(ovldcall, obj):
+        self = ovldcall.obj
+        rval = ovldcall.resolve(obj)(obj)
+        if rval is NotImplemented:
+            return self.default_hrepr_short(obj)
+        else:
+            return rval
+
+    def hrepr_short(self, obj: object):
+        if hasattr(obj, "__hrepr_short__"):
+            return obj.__hrepr_short__(self, self.H)
+        else:
+            return NotImplemented
+
+    def __call__(self, obj, **config):
+        self.state.skip_default = False
+        runner = self.with_config(config)
+        ido = id(obj)
+        if self.state.stack[ido]:
+            return runner.ref(obj, loop=True)
+
+        if self.state.registered(ido) and not runner.config.norefs:
+            return runner.ref(obj)
+
+        # Collect resources for this object
+        cls = type(obj)
+        if cls not in self.state.types_seen:
+            self.state.types_seen.add(cls)
+            resources = self.hrepr_resources[cls](cls)
+            if resources:
+                if not isinstance(resources, (tuple, list, set, frozenset)):
+                    self.state.resources.add(resources)
+                else:
+                    self.state.resources.update(resources)
+
+        # Push object on the stack to detect circular references
+        self.state.stack[ido] += 1
+        self.state.depth += 1
+
+        if (runner.config.max_depth is not None
+            and self.state.depth >= runner.config.max_depth):
+            rval = runner.hrepr_short(obj)
+        else:
+            rval = runner.hrepr(obj)
+
+        # Pop object from the stack
+        self.state.depth -= 1
+        self.state.stack[ido] -= 1
+        return rval
+
+
+class StdHrepr(Hrepr):
+
+    def global_resources(self):
+        return {self.H.style(css_hrepr)}
+
+    # Lists
+
+    def hrepr(self, xs: list):
+        return std.iterable(self, xs, before="[", after="]")
+
+    def hrepr_short(self, xs: list):
+        return self.H.span["hreprs-list"]("[...]")
+
+    # Tuples
+
+    def hrepr(self, xs: tuple):
+        return std.iterable(self, xs, before="(", after=")")
+
+    def hrepr_short(self, xs: tuple):
+        return self.H.span["hreprs-tuple"]("(...)")
+
+    # Sets
+
+    def hrepr(self, xs: (set, frozenset)):
+        return std.iterable(self, xs, before="{", after="}")
+
+    def hrepr_short(self, xs: (set, frozenset)):
+        cls = type(xs).__name__
+        return self.H.span[f"hreprs-{cls}"]("{...}")
+
+    # Dictionaries
+
+    def hrepr(self, obj: dict):
+        cls = type(obj).__name__
+        return std.instance(
+            self,
+            ("{", "}"),
+            list(obj.items()),
+            cls=f"hreprt-{cls}",
+            quote_string_keys=True,
+            short=self.config.mapping_layout == "h",
+        )
+
+    def hrepr_short(self, xs: dict):
+        cls = type(xs).__name__
+        return self.H.span[f"hreprs-{cls}"]("{...}")
+
+    # Dataclasses
+
+    def hrepr(self, obj: dataclass_without("__hrepr__")):
+        cls = type(obj).__name__
+        props = {
+            field.name: getattr(obj, field.name)
+            for field in dataclass_fields(obj)
+        }
+        return std.instance(
+            self,
+            cls,
+            props,
+            cls=f"hreprt-{cls}",
+            quote_string_keys=False,
+            delimiter="=",
+        )
+
+    def hrepr_short(self, xs: dataclass_without("__hrepr_short__")):
+        cls = type(xs).__name__
+        return self.H.span[f"hreprs-{cls}", "hrepr-short-instance"](f"{cls} ...")
+
+    # Strings
+
+    def hrepr(self, x: str):
+        if len(x) <= 15:
+            return SHORT
+        else:
+            return self.H.span[f"hreprt-str"](x)
+
+    def hrepr_short(self, x: str):
+        return self.H.span[f"hreprt-str"](
+            textwrap.shorten(x, 15, placeholder="...")
+        )
+
+    # Numbers
+
+    def hrepr_short(self, x: (int, float)):
+        return self.H.span[f"hreprt-int"](str(x))
+        # return std.standard(self, x)
+
+
+def inject_reference_numbers(hcall, node, refmap):
+    if isinstance(node, Tag):
+        node.children = [
+            inject_reference_numbers(hcall, child, refmap)
+            for child in node.children
+        ]
+        refnum = refmap.get(id(node), None)
+        if refnum is not None:
+            ref = hcall.H.span["hrepr-ref"]("#", refnum)
+            return hcall.H.div["hrepr-refbox"](ref("="), node)
+        else:
+            return node
+    else:
+        return node
+
+
+H = HTML()
+
+
+def hrepr(obj, **config):
+    hcall = StdHrepr(H=H, config=Config(config))
+    rval = hcall(obj)
+    rval = inject_reference_numbers(hcall, rval, hcall.state.make_refmap())
+    rval.resources = hcall.state.resources
+    return rval
---
+++ hrepr/std.py
@@ -0,0 +1,187 @@
+
+
+def standard(hrepr, obj, *, cls=None, tag="span"):
+    """
+    Standard representation for objects, used when there is no
+    handler for its type in type_handlers on the HRepr object,
+    and no __hrepr__ method on obj. For an object of class 'klass',
+    the result is:
+
+    ``<span class="hrepr-klass">{escape(str(obj))}</span>``
+
+    Where ``{escape(str(obj))}`` is the HTML-escaped string representation
+    of the object.
+
+    Args:
+        obj: The object to represent.
+        cls (optional): The class name for the representation. If None,
+            stdrepr will use ``'hrepr-' + obj.__class__.___name__``
+        tag (optional): The tag for the representation, defaults to
+            'span'.
+    """
+    if cls is None:
+        cls = f"hrepr-{obj.__class__.__name__}"
+    return getattr(hrepr.H, tag)[cls](str(obj))
+
+
+# def short(hrepr, obj, *, cls=None, tag="span"):
+#     """
+#     Standard short representation for objects, used for objects at
+#     a depth that exceeds ``hrepr_object.config.max_depth``. That
+#     representation is just the object's type between ``<>``s, e.g.
+#     ``<MyClass>``.
+
+#     This behavior can be overriden with a ``__hrepr_short__`` method
+#     on the object, or an entry in ``hrepr_object.type_handlers_short``.
+
+#     Args:
+#         obj: The object to represent.
+#         cls (optional): The class name for the representation. If None,
+#             stdrepr will use ``'hrepr-' + obj.__class__.___name__``
+#         tag (optional): The tag for the representation, defaults to
+#             'span'.
+#     """
+#     cls_name = obj.__class__.__name__
+#     if cls is None:
+#         cls = f"hrepr-short-{cls_name}"
+#     return getattr(hrepr.H, tag)[cls](f"<{cls_name}>")
+
+
+def iterable(hrepr, obj, *, cls=None, before=None, after=None):
+    """
+    Helper function to represent iterables. StdHRepr calls this on
+    lists, tuples, sets and frozensets, but NOT on iterables in general.
+    This method may be called to produce custom representations.
+
+    Arguments:
+        obj (iterable): The iterable to represent.
+        cls (optional): The class name for the representation. If None,
+            stdrepr will use ``'hrepr-' + obj.__class__.___name__``
+        before (optional): A string or a Tag to prepend to the elements.
+        after (optional): A string or a Tag to append to the elements.
+    """
+    if cls is None:
+        cls = f"hreprt-{obj.__class__.__name__}"
+    children = [hrepr(a) for a in obj]
+    return titled_box(hrepr, (before, after), children, "h", "h")[cls]
+
+
+def instance(
+    self,
+    title,
+    elements,
+    *,
+    cls=None,
+    short=False,
+    quote_string_keys=False,
+    delimiter=None,
+):
+    """
+    Helper function to represent objects.
+
+    Arguments:
+        title: A title string displayed above the box containing
+            the elements, or a pair of two strings that will be
+            displayed left and right (e.g. a pair of brackets).
+        elements: A dict or list of (key, value) pairs, which will be
+            displayed in a table in the order given.
+        cls: A class to give to the result.
+        short: Whether to use short or long form. Short form
+            displays the elements as ``k=v``, appended horizontally.
+            The alternative is a table, with associations stacked
+            vertically.
+        quote_string_keys: If True, string keys will be displayed
+            with quotes around them. Default is False.
+        delimiter: The character to use to separate key and
+            value. By default '↦' if quote_string_keys is True.
+    """
+    if isinstance(elements, dict):
+        elements = list(elements.items())
+
+    H = self.H
+
+    if delimiter is None and quote_string_keys is True:
+        delimiter = " ↦ "
+
+    def wrap(x):
+        if not quote_string_keys and isinstance(x, str):
+            return x
+        else:
+            return self(x)
+
+    if short:
+        contents = []
+        for k, v in elements:
+            kv = H.div["hrepr-instance-kvpair"](
+                wrap(k), delimiter or "", self(v)
+            )
+            contents.append(kv)
+    else:
+        t = H.table()["hrepr-instance-table"]
+        for k, v in elements:
+            tr = H.tr(H.td(wrap(k)))
+            if delimiter is not None:
+                tr = tr(H.td["hrepr-delimiter"](delimiter))
+            tr = tr(H.td(self(v)))
+            t = t(tr)
+        contents = [t]
+
+    title_brackets = isinstance(title, tuple) and len(title) == 2
+    horizontal = short or title_brackets
+
+    rval = titled_box(
+        self,
+        title,
+        contents,
+        "h" if title_brackets else "v",
+        "h" if short else "v",
+    )
+
+    if cls:
+        rval = rval[cls]
+
+    return rval
+
+
+def titled_box(self, titles, contents, tdir="h", cdir="h"):
+    """
+    Helper function to build a box containing a list of elements,
+    with a title above and/or below, or left and/or right of the
+    box. (e.g. a class name on top, or brackets on both sides.)
+
+    The elements given must already have been transformed into
+    Tag instances.
+
+    Arguments:
+        titles: A pair of strings to display on top and bottom
+            (if tdir=='v') or left and right (if tdir=='h').
+            If either or both titles are None, they will be
+            omitted.
+        contents: A list of Tags.
+        tdir: tdir=='h' (default) means the titles will be on
+            the left and right. tdir=='v' means they will be
+            on top and bottom.
+        cdir: cdir=='h' (default) means the contents will be
+            stacked horizontally. cdir=='v' means they will
+            be stacked vertically.
+    """
+    H = self.H
+
+    def wrapt(x):
+        return H.div["hrepr-title"](x)
+
+    rval = H.div[f"hrepr-titled-{tdir}"]
+    contents = H.div[f"hrepr-contents-{cdir}"].fill(contents)
+
+    if isinstance(titles, tuple) and len(titles) == 2:
+        open, close = titles
+    else:
+        open, close = titles, None
+
+    if open:
+        rval = rval(wrapt(open))
+    rval = rval(contents)
+    if close:
+        rval = rval(wrapt(close))
+
+    return rval

99dcc23e615b6e0c4f67219df02973f3fd56e2bf
Olivier Breuleux <breuleux@gmail.com>
2022-11-18 12:55:40 -0500

    Finalize coverage, improve constructor interface

--- tests/test_hgen.py
+++ tests/test_hgen.py
@@ -1,11 +1,20 @@
+from itertools import count
 from types import FunctionType, MethodType
 from typing import Union
 
 import pytest
 
-from hrepr import H, embed, standard_html
+from hrepr import H, embed
+from hrepr import hgen as hmodule
+from hrepr import standard_html
 from hrepr.hgen import HTMLGenerator
 from hrepr.resource import JSExpression, Resource
+
+
+@pytest.fixture(autouse=True)
+def reset_id_counter():
+    hmodule.id_counter = count()
+
 
 incrementer_script = H.script(
     """
@@ -31,9 +40,14 @@ def test_constructor_global_symbol(file_regression):
         H.button(
             "ERROR!",  # Will only show if the button's text is not set by the script
             style="width:100px;",
-            constructor={"symbol": "Counter", "options": {"increment": 3,}},
+            id="butt",
+            __constructor={
+                "script": incrementer_script,
+                "symbol": "Counter",
+                "options": {"increment": 3},
+            },
         ),
-    ).fill(resources=incrementer_script)
+    )
 
     file_regression.check(str(node.as_page()), extension=".html")
 
@@ -44,14 +58,16 @@ def test_constructor_multiple(file_regression):
         H.button(
             "ERROR!",
             style="width:100px;",
-            constructor={"symbol": "Counter", "options": {"increment": 2,}},
+            __constructor={"symbol": "Counter", "options": {"increment": 2}},
+            __resources=incrementer_script,
         ),
         H.button(
             "ERROR!",
             style="width:100px;",
-            constructor={"symbol": "Counter", "options": {"increment": 3,}},
+            __constructor={"symbol": "Counter", "options": {"increment": 3}},
+            __resources=[incrementer_script],
         ),
-    ).fill(resources=incrementer_script)
+    )
 
     file_regression.check(str(node.as_page()), extension=".html")
 
@@ -76,7 +92,7 @@ def test_constructor_cytoscape(file_regression):
         H.h2("This should show an interactive graph."),
         H.div(
             style="width:500px;height:500px;border:1px solid black;",
-            constructor={
+            __constructor={
                 "module": "https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.23.0/cytoscape.esm.min.js",
                 "arguments": {
                     "container": H.self(),
@@ -92,6 +108,28 @@ def test_constructor_cytoscape(file_regression):
                     "layout": {"name": "cose"},
                 },
             },
+        ),
+    )
+
+    file_regression.check(str(node.as_page()), extension=".html")
+
+
+def test_constructor_module(file_regression):
+    node = H.div(
+        H.h2("The buttons should increment by 2 and 3 respectively."),
+        H.h4(
+            "Note: this will NOT work when browsing the file directly, ",
+            "view using a server e.g. with `python -m http.server`.",
+        ),
+        H.button(
+            "ERROR!",
+            style="width:100px;",
+            __constructor={"module": "./counter.esm.js", "symbol": "bytwo"},
+        ),
+        H.button(
+            "ERROR!",
+            style="width:100px;",
+            __constructor={"module": "./counter.esm.js", "symbol": "by.three"},
         ),
     )
 
@@ -129,7 +167,7 @@ def test_resource_embed_function(customgen, file_regression):
 
 def test_resource_embed_tag(customgen, file_regression):
     inp = H.input(id="inp")
-    box = H.div(id="here", style="padding:5px;border:solid blue")
+    box = H.div(id="here", style="padding:5px;border:solid blue;height:100px;")
 
     node = H.div(
         H.h3("The button should append the text to the blue box"),
@@ -144,6 +182,3 @@ def test_resource_embed_tag(customgen, file_regression):
     )
 
     file_regression.check(customgen(node), extension=".html")
-
-
-# def test_resource_module_extraction

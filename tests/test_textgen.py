6923642b7dd5adf0b1cd7a2154e839672f525a32
Olivier Breuleux <breuleux@gmail.com>
2022-11-18 16:55:36 -0500

    Add test to recover missed coverage

--- tests/test_textgen.py
+++ tests/test_textgen.py
@@ -1,4 +1,29 @@
+from hrepr.embed import js_embed
 from hrepr.textgen import Context, Text
+
+
+def test_max_col(file_regression):
+    dct = {
+        "module": "https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.23.0/cytoscape.esm.min.js",
+        "arguments": {
+            "elements": [
+                {"data": {"id": "A"}},
+                {"data": {"id": "B"}},
+                {"data": {"id": "C"}},
+                {"data": {"source": "A", "target": "B"}},
+                {"data": {"source": "B", "target": "C"}},
+                {"data": {"source": "C", "target": "A"}},
+            ],
+            "layout": {"name": "cose"},
+        },
+    }
+    file_regression.check(
+        str(
+            js_embed(dct).to_string(
+                max_col=80, tabsize=4, offset=0, line_offset=0
+            )
+        )
+    )
 
 
 def test_overflow_whitespace():

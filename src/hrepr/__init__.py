a90c6c760eafc999bbe05bbe2e796abbd7cbc493
Olivier Breuleux <breuleux@gmail.com>
2024-08-06 14:37:07 -0400

    Add __all__ statement

--- src/hrepr/__init__.py
+++ src/hrepr/__init__.py
@@ -1,11 +1,11 @@
 """Extensible HTML representation for Python objects."""
 
 from . import elements, h
-from .core import Config, Hrepr, HreprState, Interface, StdHrepr  # noqa: F401
-from .h import HTML, H, HType, Tag  # noqa: F401
+from .core import Config, Hrepr, HreprState, Interface, StdHrepr
+from .h import HTML, H, HType, Tag
 from .hgen import BlockGenerator, HTMLGenerator, standard_html
-from .j import J, Returns  # noqa: F401
-from .resource import JSExpression, Resource  # noqa: F401
+from .j import J, Returns
+from .resource import JSExpression, Resource
 
 returns = Returns
 h.standard_html = standard_html
@@ -17,3 +17,26 @@ config_defaults = {
 }
 
 hrepr = Interface(StdHrepr, **config_defaults)
+
+__all__ = [
+    "BlockGenerator",
+    "Config",
+    "H",
+    "HTML",
+    "HTMLGenerator",
+    "HType",
+    "Hrepr",
+    "HreprState",
+    "Interface",
+    "J",
+    "JSExpression",
+    "Resource",
+    "Returns",
+    "StdHrepr",
+    "Tag",
+    "elements",
+    "h",
+    "hrepr",
+    "returns",
+    "standard_html",
+]
